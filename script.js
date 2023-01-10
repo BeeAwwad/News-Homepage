3b46fbbf09ca518ac49c9d2b0b1cc72459f7278d
BeeAwwad <113793663+BeeAwwad@users.noreply.github.com>
2023-01-10 05:21:58 +0100

    Trying to exlude the navication from the blur effect of the toggle button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const mainNav = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.mobile-nav-toggle');
+const body = document.querySelector('body');
 
 navToggle.addEventListener("click", () => {
     const visibility = mainNav.getAttribute("data-visible");
@@ -7,9 +8,11 @@ navToggle.addEventListener("click", () => {
     if (visibility === "false") {
         mainNav.setAttribute("data-visible", true);
         navToggle.setAttribute("aria-expanded", "true");
+        body.classList.add('blur');
     }
     else if (visibility === "true") {
         mainNav.setAttribute("data-visible", "false");
         navToggle.setAttribute("aria-expanded", false);
+        body.classList.remove('blur');
     }
 });
