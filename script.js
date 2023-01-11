0323b9c2b9517d4c470cbcbb83eaed049bd0ae49
BeeAwwad <113793663+BeeAwwad@users.noreply.github.com>
2023-01-11 10:06:01 +0100

    Fix the bluring effect in mobile view

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const mainNav = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.mobile-nav-toggle');
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
+const mainNews = document.querySelector('.main-story');
+const moreNews = document.querySelector('.more-news');
+const topOne = document.querySelector('.top-1');
+const topTwo = document.querySelector('.top-2');
+const topThree = document.querySelector('.top-3');
+const logo = document.querySelector('.logo');
 
 navToggle.addEventListener("click", () => {
     const visibility = mainNav.getAttribute("data-visible");
@@ -8,11 +14,21 @@ navToggle.addEventListener("click", () => {
     if (visibility === "false") {
         mainNav.setAttribute("data-visible", true);
         navToggle.setAttribute("aria-expanded", "true");
-        body.classList.add('blur');
+        mainNews.classList.add('blur');
+        moreNews.classList.add('blur');
+        topOne.classList.add('blur');
+        topTwo.classList.add('blur');
+        topThree.classList.add('blur');
+        logo.classList.add('blur');
     }
     else if (visibility === "true") {
         mainNav.setAttribute("data-visible", "false");
         navToggle.setAttribute("aria-expanded", false);
-        body.classList.remove('blur');
+        mainNews.classList.remove('blur');
+        moreNews.classList.remove('blur');
+        topOne.classList.remove('blur');
+        topTwo.classList.remove('blur');
+        topThree.classList.remove('blur');
+        logo.classList.remove('blur');
     }
 });
